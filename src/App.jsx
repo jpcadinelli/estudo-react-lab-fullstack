5cb0bd6949138da7d1332b2393a8ab7387213c0e
jpcadinelli <jpcadinelli@gmail.com>
2025-11-25 17:32:54 -0300

    feat: utilizando css, e imagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function Saudacao({ nome }) {
-    return <h2>Bem-vindo(a), { nome }!</h2>
+    return <h2 className="titulo-dois" >Bem-vindo(a), { nome }!</h2>
 }
 
 function App() {
     return (
         <div >
+            <img src={ reactLogo } Alt="Logo do react" />
             <Saudacao nome={ "Tássio" } />
             <Saudacao nome={ "Turma UNIVASS" } />
         </div >
